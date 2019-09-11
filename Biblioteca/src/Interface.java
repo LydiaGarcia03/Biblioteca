fb41659773e43cc13f9286b34472223659f70bb1
Lydia Garcia <lydiagarcia.chan@gmail.com>
2019-09-11 08:07:13 -0300

    Primeira interface que permite redirecionamento para login e cadastro

--- Biblioteca/src/Interface.java
+++ Biblioteca/src/Interface.java
@@ -1,9 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class Interface {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		JFrame frame = new JFrame();
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblSistemaBibliotecrio = new JLabel("Sistema bibliotec\u00E1rio");
+		lblSistemaBibliotecrio.setFont(new Font("Garamond", Font.PLAIN, 18));
+		lblSistemaBibliotecrio.setBounds(90, 34, 165, 32);
+		frame.getContentPane().add(lblSistemaBibliotecrio);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(115, 108, 89, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		JButton btnCadastrese = new JButton("Cadastre-se");
+		btnCadastrese.setBounds(104, 153, 111, 23);
+		frame.getContentPane().add(btnCadastrese);
+		
 	}
-
 }
